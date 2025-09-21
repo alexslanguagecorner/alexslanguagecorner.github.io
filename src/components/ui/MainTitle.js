bb9fb6593a18e19139d73f2892e5746d7d1a20bc
Olga O <42141921+olgamadej@users.noreply.github.com>
2025-09-21 20:13:54 +0200

    media hero update

--- src/components/ui/MainTitle.js
+++ src/components/ui/MainTitle.js
@@ -1,29 +1,33 @@
 import React from "react";
-import './MainTitle.scss';
+import "./MainTitle.scss";
 import logo from "../../img/logo.png";
 
 const MainTitle = () => {
   return (
     <div className="center">
-      <div className="logo-container">        
+      <div className="logo-container">
         <div className="logo-first-line">
           <h2>&nbsp;&nbsp;&nbsp;&nbsp;Welcome to </h2>
-          <a className="logo" href="#"> <img src={logo} alt="Alex's Language Corner"></img> </a>
+          <div className="logo">
+            {" "}
+            <img src={logo} alt="Alex's Language Corner"></img>{" "}
+          </div>
         </div>
       </div>
       <div className="sub-tagline">
         <h3>Learn&nbsp; French or English&nbsp; with Ease</h3>
       </div>
       <div className="sub-about">
-      <button className="free-materials-button">
+        <button className="free-materials-button">
           Explore Free Materials
-      </button>
-      <p className="content-menu-text">
-      « Le langage est la peinture de nos pensées. » <br />
-“Language is the painting of our thoughts.”<br/>
-<br/>
-<span>Molière</span> 
-      </p>
+        </button>
+        <p className="content-menu-text">
+          « Le langage est la peinture de nos pensées. » <br />
+          “Language is the painting of our thoughts.”
+          <br />
+          <br />
+          <span>Molière</span>
+        </p>
       </div>
     </div>
   );
